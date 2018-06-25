8f4283e86fd349ff40ca2b9979c9ed8193076a4a
Myakkatt <myao_03@icloud.com>
2018-06-25 15:57:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,33 @@
+class Song
+  
+  def initialize(title)
+    @title= title
+  end
+  
+  def title
+    @title
+  end
+  
+  def artists= (artists)
+    @artists = artists
+  end
+  
+  def artists
+    @artists
+  end
+  
+  def genres= (genres)
+    @genres = genres
+  end
+  
+  def genres
+    @genres
+  end
+  
+Song.artists
+# => ["Jay-Z", "Drake", "Beyonce"]
 
+Song.genres 
+# => ["Rap", "Pop"]
+
+end
